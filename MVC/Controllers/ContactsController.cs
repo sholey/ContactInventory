de2589eeba96745c36609c599027c8dbb97ad606
Saurabh Holey <SHOLEY@amdocs.com>
2021-05-10 15:26:28 +0530

    Add project files.

---
+++ MVC/Controllers/ContactsController.cs
@@ -0,0 +1,57 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            IEnumerable<mvcContactModel> contactList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Contacts").Result;
+            contactList = response.Content.ReadAsAsync<IEnumerable<mvcContactModel>>().Result;
+            return View(contactList);
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new mvcContactModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Contacts/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcContactModel>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcContactModel contact)
+        {
+            if (contact.Id  == 0 )
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Contacts", contact).Result;
+                TempData["Success"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Contacts/" + contact.Id, contact).Result;
+                TempData["Success"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Contacts/" + id.ToString()).Result;
+            TempData["Success"] = "Delete Successfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVC/Models/mvcContactModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class mvcContactModel
+    {
+       
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        public Nullable<decimal> PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
